4739c6e1cf0578fec2404f6d19e4f7c1d654c2c0
Joshua Franklin <jfranklin1607@gmail.com>
2019-08-20 13:11:47 -0400

    screens-app

---
+++ Screens/RedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RedViewController.swift
+//  Screens
+//
+//  Created by Joshua Franklin on 8/19/19.
+//  Copyright © 2019 AMSU. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    @IBAction func unwindToRed(_ sender: UIStoryboardSegue) {
+        
+    }
+
+}
